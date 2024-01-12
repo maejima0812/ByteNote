9d03719935bf8250f0d0a3eaf3c582b155b33726
maejima <botiyuta1016@gmail.com>
2024-01-12 15:22:11 +0900

    吉野家

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -34,7 +34,7 @@ public function store()
 }
    public function getByLimit(int $limit_count = 10)
 {
-    // updated_atで降順に並べたあと、limitで件数制限をかける
+    
     return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
 }
  
